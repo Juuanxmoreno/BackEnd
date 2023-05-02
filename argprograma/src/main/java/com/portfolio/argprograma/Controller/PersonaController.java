344d5b01c52a6a5ee48920d1f828e891edd9b274
Juan Cruz Moreno <111616899+Juuanxmoreno@users.noreply.github.com>
2023-05-02 17:46:34 -0300

    BackEnd con JWT

--- argprograma/src/main/java/com/portfolio/argprograma/Controller/PersonaController.java
+++ argprograma/src/main/java/com/portfolio/argprograma/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.argprograma.Entity.Persona;
 import com.portfolio.argprograma.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author ricar
- */
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
@@ -32,18 +29,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                 @RequestParam("nombre") String nuevoNombre,
