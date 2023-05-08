8f9d07e3d2141d136d07b052c406c38f889c4b21
Juan Cruz Moreno <111616899+Juuanxmoreno@users.noreply.github.com>
2023-05-08 20:28:19 -0300

    Cambios en PersonaController

--- argprograma/src/main/java/com/portfolio/argprograma/Controller/PersonaController.java
+++ argprograma/src/main/java/com/portfolio/argprograma/Controller/PersonaController.java
@@ -16,13 +16,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/personas")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
 
@@ -44,7 +45,13 @@ public class PersonaController {
         Persona persona = impPersonaService.getOne(id).get();
         return new ResponseEntity(persona, HttpStatus.OK);
     }
-
+    
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+        impPersonaService.save(persona);
+        return "La persona fue creada correctamente";
+    }
+    
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtopersona) {
         if (!impPersonaService.existById(id)) {
